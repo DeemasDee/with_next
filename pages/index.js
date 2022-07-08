ec74ac63e26b23618dd48df0acfb67cac3dce44b
DeemasDee <deemasdee@gmail.com>
2022-07-08 08:13:14 +0700

    first upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{paddingLeft: 30 + '%', fontFamily: 'Times New Roman'}}>Frontend CRUD</h1><br />
+            <b><p>Nama : M ROMAN R.E</p>
+            <p>NIM : 1120101919</p></b>
+    </div>
+  )
+}
